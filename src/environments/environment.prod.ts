b27f200f8cf58b2030119776e323c7c4762aa17b
hocinebouarara <hocinbouarara08@gmail.com>
2023-05-30 15:46:45 +0100

    added environment firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDR4QIW0dxAUHukyrhj14w8s9Cb0Q-hcMY',
+    authDomain: 'ecommerce-app-c0503.firebaseapp.com',
+    projectId: 'ecommerce-app-c0503',
+    storageBucket: 'ecommerce-app-c0503.appspot.com',
+    messagingSenderId: '99896913825',
+    appId: '1:99896913825:web:23dfd81f0fec24ee75a5e0',
+    measurementId: 'G-BBY7GKQRH3',
+  },
+};
