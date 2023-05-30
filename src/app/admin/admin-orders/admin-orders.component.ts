cd73fa1a9c209c8a8953c5ee521a0571411291bc
hocinebouarara <hocinbouarara08@gmail.com>
2023-05-30 17:31:10 +0100

    initi admin orders component

---
+++ src/app/admin/admin-orders/admin-orders.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-orders',
+  templateUrl: './admin-orders.component.html',
+  styleUrls: ['./admin-orders.component.css']
+})
+export class AdminOrdersComponent {
+
+}
