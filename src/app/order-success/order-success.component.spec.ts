533de6eaa5ad4ddc8cef6c31f8d899483100172f
hocinebouarara <hocinbouarara08@gmail.com>
2023-05-30 17:42:47 +0100

    init order-success component

---
+++ src/app/order-success/order-success.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OrderSuccessComponent } from './order-success.component';
+
+describe('OrderSuccessComponent', () => {
+  let component: OrderSuccessComponent;
+  let fixture: ComponentFixture<OrderSuccessComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OrderSuccessComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(OrderSuccessComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
